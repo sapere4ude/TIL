01a7a9e2c38f505325d13077492ddc857410c3df
sapere4ude <48309864+sapere4ude@users.noreply.github.com>
2021-02-07 14:13:05 +0900

    Objective-C NSLog 사용방법

---
+++ HelloWorld/HelloWorld/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HelloWorld
+//
+//  Created by sapere4ude on 2021/02/07.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, Objective C!");
+    }
+    return 0;
+}
