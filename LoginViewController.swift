699c638382bdce3e8ece7fdf23315d850f4ccc36
sapere4ude <48309864+sapere4ude@users.noreply.github.com>
2021-02-13 00:50:53 +0900

    Add files via upload

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -8,81 +8,75 @@
 import UIKit
 import AuthenticationServices
 
-class LoginViewController: UIViewController, ASAuthorizationControllerDelegate, ASAuthorizationControllerPresentationContextProviding {
-    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
-        return self.view.window!
-    }
+class LoginViewController: UIViewController, ASAuthorizationControllerDelegate {
     
-    @IBOutlet weak var loginProviderStackView: UIStackView!
-    
-    private let loginButton: KOLoginButton = {
-        let button = KOLoginButton()
-        button.addTarget(self, action: #selector(touchUpLoginButton(_:)), for: .touchUpInside)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-      }()
-
-    // Apple 로그인
-    func setupProviderLoginView() {
-      let authorizationButton = ASAuthorizationAppleIDButton()
-      authorizationButton.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
-      self.loginProviderStackView.addArrangedSubview(authorizationButton)
-    }
-    
-    
-      override func viewDidLoad() {
+    override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        layout()
-        setupProviderLoginView()
-        print("check")
-      }
-      
+        view.addSubview(appleLoginBtn)
+        view.addSubview(kakaoLoginButton)
+    }
+    
+    private let appleLoginBtn: ASAuthorizationAppleIDButton = {
+       let button = ASAuthorizationAppleIDButton()
+        button.addTarget(self, action: #selector(handleAuthorizationAppleIDButtonPress), for: .touchUpInside)
+        return button
+    }()
+    
+    private let kakaoLoginButton: KOLoginButton = {
+    let button = KOLoginButton()
+    button.addTarget(self, action: #selector(touchUpLoginButton(_:)), for: .touchUpInside)
+    button.translatesAutoresizingMaskIntoConstraints = false
+    return button
+    }()
+    
+    // Apple Login
+    @objc func handleAuthorizationAppleIDButtonPress() {
+        let request = ASAuthorizationAppleIDProvider().createRequest()
+        request.requestedScopes = [.fullName, .email]
+        let controller = ASAuthorizationController(authorizationRequests: [request])
+        
+        controller.delegate = self
+        controller.presentationContextProvider = self
+        controller.performRequests()
+    }
+    
     // Kakao Login
-      @objc private func touchUpLoginButton(_ sender: UIButton) {
+    @objc private func touchUpLoginButton(_ sender: UIButton) {
         guard let session = KOSession.shared() else {
-          return
+            return
         }
         
         if session.isOpen() {
-          session.close()
+            session.close()
         }
         
         session.open { (error) in
-          if error != nil || !session.isOpen() { return }
-          KOSessionTask.userMeTask(completion: { (error, user) in
-            guard let user = user,
-                  let email = user.account?.email,
-                  let nickname = user.nickname else { return }
-            
-            let mainVC = MainViewController()
-            mainVC.emailLabel.text = email
-            mainVC.nicnameLabel.text = nickname
-            
-            self.present(mainVC, animated: false, completion: nil)
-          })
+            if error != nil || !session.isOpen() { return }
+            KOSessionTask.userMeTask(completion: { (error, user) in
+                guard let user = user,
+                      let email = user.account?.email,
+                      let nickname = user.nickname else { return }
+                
+                let mainVC = MainViewController()
+                mainVC.emailLabel.text = email
+                mainVC.nicnameLabel.text = nickname
+                
+                self.present(mainVC, animated: false, completion: nil)
+            })
         }
-      }
-
-    // Apple 로그인 버튼 클릭
-    @objc func handleAuthorizationAppleIDButtonPress() {
-      let appleIDProvider = ASAuthorizationAppleIDProvider()
-      let request = appleIDProvider.createRequest()
-      request.requestedScopes = [.fullName, .email]
-            
-      let authorizationController = ASAuthorizationController(authorizationRequests: [request])
-      authorizationController.delegate = self
-      authorizationController.presentationContextProvider = self
-      authorizationController.performRequests()
     }
     
-      private func layout() {
-        let guide = view.safeAreaLayoutGuide
-        view.addSubview(loginButton)
-        
-        loginButton.leadingAnchor.constraint(equalTo: guide.leadingAnchor, constant: 20).isActive = true
-        loginButton.trailingAnchor.constraint(equalTo: guide.trailingAnchor, constant: -20).isActive = true
-        loginButton.bottomAnchor.constraint(equalTo: guide.bottomAnchor, constant: -30).isActive = true
-        loginButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
-      }
+    // layout setting
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        appleLoginBtn.frame = CGRect(x: 30, y: view.bounds.height/2, width: view.bounds.width-50, height: 50)
+        kakaoLoginButton.frame = CGRect(x: 30, y: (view.bounds.height/2)+70, width: view.bounds.width-50, height: 50)
+    }
+}
+
+extension LoginViewController: ASAuthorizationControllerPresentationContextProviding {
+    
+    func presentationAnchor(for controller: ASAuthorizationController) -> ASPresentationAnchor {
+        return self.view.window!
+    }
 }
